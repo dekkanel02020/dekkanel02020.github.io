6dbe2b92883b554359a593dec8e820ec67c4c263
dekkanel02020 <63162973+dekkanel02020@users.noreply.github.com>
2020-05-15 12:14:23 +0200

    slide

--- LaBottegaDelFabbro_New/main.js
+++ LaBottegaDelFabbro_New/main.js
@@ -46,3 +46,22 @@ menu.addEventListener("click",function(){
     
    
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    loop:true
+  });
